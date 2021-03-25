448afc9ad97f3b31eebd217fc8a8f804e7747134
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2021-03-25 15:12:20 +0100

    cykusz: Call __dlapi_enter on exec

--- sysdeps/cykusz/generic/entry.cpp
+++ sysdeps/cykusz/generic/entry.cpp
@@ -7,6 +7,7 @@
 void __mlibc_initLocale();
 
 extern "C" uintptr_t *__dlapi_entrystack();
+extern "C" void __dlapi_enter(uintptr_t *);
 
 extern char **environ;
 static mlibc::exec_stack_data __mlibc_stack_data;
@@ -18,21 +19,16 @@ struct LibraryGuard {
 static LibraryGuard guard;
 
 LibraryGuard::LibraryGuard() {
- 	//__mlibc_initLocale();
+ 	__mlibc_initLocale();
      
-     
- 
- 	//// Parse the exec() stack.
- 	//mlibc::parse_exec_stack(__dlapi_entrystack(), &__mlibc_stack_data);
- 	//mlibc::set_startup_data(__mlibc_stack_data.argc, __mlibc_stack_data.argv,
- 	//		__mlibc_stack_data.envp);
+ 	// Parse the exec() stack.
+ 	mlibc::parse_exec_stack(__dlapi_entrystack(), &__mlibc_stack_data);
+ 	mlibc::set_startup_data(__mlibc_stack_data.argc, __mlibc_stack_data.argv,
+ 			__mlibc_stack_data.envp);
 }
 
 extern "C" void __mlibc_entry(uintptr_t *entry_stack, int (*main_fn)(int argc, char *argv[], char *env[])) {
-	__mlibc_run_constructors();
-	mlibc::parse_exec_stack(entry_stack, &__mlibc_stack_data);
- 	mlibc::set_startup_data(__mlibc_stack_data.argc, __mlibc_stack_data.argv,
- 				__mlibc_stack_data.envp);
+	__dlapi_enter(entry_stack);
 	auto result = main_fn(__mlibc_stack_data.argc, __mlibc_stack_data.argv, __mlibc_stack_data.envp);
 	exit(result);
 }
