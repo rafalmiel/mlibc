42198375d68f3769232ddaf09ae47c65736894a7
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2021-03-25 14:40:35 +0100

    cykusz: Support program arguments

--- options/posix/include/mlibc/posix-sysdeps.hpp
+++ options/posix/include/mlibc/posix-sysdeps.hpp
@@ -92,7 +92,7 @@ int sys_close(int fd);
 [[gnu::weak]] int sys_sleep(time_t *secs, long *nanos);
 [[gnu::weak]] int sys_fork(pid_t *child);
 [[gnu::weak]] int sys_clone(void *entry, void *user_arg, void *tcb, pid_t *pid_out);
-[[gnu::weak]] int sys_execve(const char *path, char *const argv[], char *const envp[]);
+	      int sys_execve(const char *path, char *const argv[], char *const envp[]);
 [[gnu::weak]] int sys_pselect(int num_fds, fd_set *read_set, fd_set *write_set,
 		fd_set *except_set, const struct timespec *timeout, const sigset_t *sigmask, int *num_events);
 [[gnu::weak]] int sys_getrusage(int scope, struct rusage *usage);
