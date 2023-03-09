f9aa1088bb9247eb61aa69ee64c23a97b1aed327
Rafal Mielniczuk <rafal.mielniczuk2@gmail.com>
2023-03-09 20:24:21 +0000

    cykusz 3.0

---
+++ sysdeps/cykusz/generic/filesystem.cpp
@@ -0,0 +1,396 @@
+#include <cykusz/syscall.h>
+
+#include <asm/ioctls.h>
+#include <bits/ensure.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <dirent.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <string.h>
+
+#include <mlibc/all-sysdeps.hpp>
+#include <mlibc/debug.hpp>
+
+namespace mlibc {
+
+    int sys_write(int fd, const void *buffer, size_t count, ssize_t *written) {
+        long ret = syscalln3(SYS_WRITE, fd, (uintptr_t) buffer, count);
+
+        if (ret < 0)
+            return -ret;
+
+        *written = ret;
+        return 0;
+    }
+
+    int sys_read(int fd, void *buf, size_t count, ssize_t *bytes_read) {
+        long ret = syscalln3(SYS_READ, fd, (uintptr_t) buf, count);
+
+        if (ret < 0)
+            return -ret;
+
+        *bytes_read = ret;
+        return 0;
+    }
+
+    int sys_pwrite(int fd, const void *buffer, size_t count, off_t off, ssize_t *written) {
+        long ret = syscalln4(SYS_PWRITE, fd, (uintptr_t) buffer, count, (uint64_t) off);
+
+        if (ret < 0)
+            return -ret;
+
+        *written = ret;
+
+        return 0;
+    }
+
+    int sys_pread(int fd, void *buf, size_t count, off_t off, ssize_t *bytes_read) {
+        long ret = syscalln4(SYS_PREAD, fd, (uintptr_t) buf, count, (uint64_t) off);
+        if (ret < 0)
+            return -ret;
+
+        *bytes_read = ret;
+
+        return 0;
+    }
+
+    int sys_seek(int fd, off_t offset, int whence, off_t *new_offset) {
+        ssize_t res = syscalln3(SYS_SEEK, (uint64_t) fd, (uint64_t) offset, (uint64_t) whence);
+
+        if (res < 0)
+            return -res;
+
+        *new_offset = res;
+
+        return 0;
+    }
+
+
+    int sys_open(const char *filename, int flags, mode_t mode, int *fd) {
+        ssize_t res = syscalln4(SYS_OPEN, (uint64_t) AT_FDCWD, (uint64_t) filename, (uint64_t) strlen(filename),
+                                (uint64_t) flags);
+
+        if (res < 0)
+            return -res;
+
+        *fd = res;
+
+        return 0;
+    }
+
+    int sys_openat(int dirfd, const char *filename, int flags, mode_t mode, int *fd) {
+        ssize_t res = syscalln4(SYS_OPEN, (uint64_t) dirfd, (uint64_t) filename, (uint64_t) strlen(filename),
+                                (uint64_t) flags);
+
+        if (res < 0)
+            return -res;
+
+        *fd = res;
+
+        return 0;
+    }
+
+    int sys_close(int fd) {
+        ssize_t res = syscalln1(SYS_CLOSE, (uint64_t) fd);
+
+        if (res < 0)
+            return -res;
+
+        return 0;
+    }
+
+    int sys_access(const char *path, int mode) {
+        ssize_t ret = syscalln5(SYS_ACCESS, (uint64_t) AT_FDCWD, (uint64_t) path, (uint64_t) strlen(path),
+                                (uint64_t) mode, 0);
+
+        if (ret < 0) {
+            return -ret;
+        }
+
+        return 0;
+    }
+
+    int sys_fsync(int fd) {
+        ssize_t ret = syscalln1(SYS_FSYNC, (uint64_t) fd);
+
+        if (ret < 0) {
+            return -ret;
+        }
+
+        return 0;
+    }
+
+    int sys_ftruncate(int fd, size_t size) {
+        ssize_t ret = syscalln2(SYS_TRUNCATE, (uint64_t)fd, (uint64_t)size);
+
+        if (ret < 0) {
+            return -ret;
+        }
+
+        return 0;
+    }
+
+    int sys_faccessat(int dirfd, const char *pathname, int mode, int flags) {
+        ssize_t ret = syscalln5(SYS_ACCESS, (uint64_t) dirfd, (uint64_t) pathname, (uint64_t) strlen(pathname),
+                                (uint64_t) mode, (uint64_t) flags);
+
+        if (ret < 0) {
+            return -ret;
+        }
+
+        return 0;
+    }
+
+    int sys_stat(fsfd_target fsfdt, int fd, const char *path, int flags, struct stat *statbuf) {
+        (void) flags;
+        ssize_t res;
+
+        switch (fsfdt) {
+            case fsfd_target::fd:
+                res = syscalln5(SYS_STAT, (uint64_t) fd, 0, 0, (uint64_t) statbuf, (uint64_t)flags);
+                break;
+            case fsfd_target::path:
+                res = syscalln5(SYS_STAT, (uint64_t)AT_FDCWD, (uint64_t) path, (uint64_t) strlen(path), (uint64_t) statbuf, (uint64_t)flags);
+                break;
+            case fsfd_target::fd_path:
+                res = syscalln5(SYS_STAT, (uint64_t)fd, (uint64_t)path, (uint64_t) strlen(path), (uint64_t) statbuf, (uint64_t)flags);
+                break;
+        }
+
+        if (res < 0) {
+            return -res;
+        }
+
+        return 0;
+    }
+
+    int sys_ioctl(int fd, unsigned long request, void *arg, int *result) {
+        ssize_t res = syscalln3(SYS_IOCTL, (uint64_t) fd, (uint64_t) request, (uint64_t) arg);
+
+        if (res < 0) {
+            return -res;
+        }
+
+        *result = res;
+
+        return 0;
+    }
+
+    int sys_isatty(int fd) {
+        uint64_t grp;
+        int res;
+        if (sys_ioctl(fd, TIOCGPGRP, &grp, &res) != ENOTTY) {
+            return 0;
+        } else {
+            return ENOTTY;
+        }
+    }
+
+
+#ifndef MLIBC_BUILDING_RTDL
+
+    int sys_ttyname(int fd, char *buf, size_t size) {
+        strncpy(buf, "/dev/tty", size);
+
+        return 0;
+    }
+
+#endif
+
+    int sys_tcgetattr(int fd, struct termios *attr) {
+        int ret;
+
+        if (int r = sys_ioctl(fd, TCGETS, attr, &ret) != 0) {
+            return r;
+        }
+
+        return ret;
+    }
+
+    int sys_tcsetattr(int fd, int optional_action, const struct termios *attr) {
+        int ret;
+        int action;
+
+        switch (optional_action) {
+            case TCSANOW:
+                optional_action = TCSETS;
+                break;
+            case TCSADRAIN:
+                optional_action = TCSETSW;
+                break;
+            case TCSAFLUSH:
+                optional_action = TCSETSF;
+                break;
+            default:
+                optional_action = TCSETS;
+                break;
+        }
+
+        if (int r = sys_ioctl(fd, optional_action, (void *) attr, &ret) != 0) {
+            return r;
+        }
+
+        return ret;
+    }
+
+    int sys_poll(struct pollfd *fds, nfds_t count, int timeout, int *num_events) {
+        ssize_t ret = syscalln3(SYS_POLL, (uint64_t)fds, (uint64_t)count, (uint64_t)timeout);
+
+        if (ret < 0) {
+            return -ret;
+        }
+
+        *num_events = ret;
+
+        return 0;
+    }
+
+    int sys_mkdir(const char *path, mode_t mode) {
+        ssize_t ret = syscalln2(SYS_MKDIR, (uint64_t) path, (uint64_t) strlen(path));
+
+        if (ret < 0) {
+            return -ret;
+        }
+
+        return 0;
+    }
+
+    int sys_rmdir(const char *path) {
+        ssize_t ret = syscalln2(SYS_RMDIR, (uint64_t) path, (uint64_t) strlen(path));
+
+        if (ret < 0) {
+            return -ret;
+        }
+
+        return 0;
+    }
+
+    int sys_link(const char *srcpath, const char *destpath) {
+        return -1;
+    }
+
+    int sys_unlink(const char *path) {
+        ssize_t ret = syscalln4(SYS_UNLINK, (uint64_t) AT_FDCWD, (uint64_t) path, (uint64_t) strlen(path), 0);
+
+        if (ret < 0) {
+            return -ret;
+        }
+
+        return 0;
+    }
+
+    int sys_unlinkat(int fd, const char *path, int flags) {
+        ssize_t ret = syscalln4(SYS_UNLINK, (uint64_t) fd, (uint64_t) path, (uint64_t) strlen(path), (uint64_t) flags);
+
+        if (ret < 0) {
+            return -ret;
+        }
+
+        return 0;
+    }
+
+    int sys_read_entries(int handle, void *buffer, size_t max_size, size_t *bytes_read) {
+        ssize_t res = syscalln3(SYS_READDIR, (uint64_t) handle, (uint64_t) buffer, (uint64_t) max_size);
+
+        if (res < 0)
+            return -res;
+
+        *bytes_read = res;
+
+        return 0;
+    }
+
+    int sys_open_dir(const char *path, int *handle) {
+        ssize_t res = syscalln4(SYS_OPEN, (uint64_t) AT_FDCWD, (uint64_t) path, (uint64_t) strlen(path), O_DIRECTORY);
+
+        if (res < 0)
+            return -res;
+
+        *handle = res;
+
+        return 0;
+    }
+
+    int sys_rename(const char *path, const char *new_path) {
+        return -1;
+    }
+
+    int sys_readlink(const char *path, void *buffer, size_t max_size, ssize_t *length) {
+        ssize_t res = syscalln5(SYS_READLINK, (uint64_t) path, (uint64_t)strlen(path), (uint64_t)buffer, (uint64_t)max_size, (uint64_t)length);
+
+        if (res < 0) {
+            return -res;
+        }
+
+        return 0;
+    }
+
+    int sys_dup(int fd, int flags, int *newfd) {
+        ssize_t res = syscalln2(SYS_DUP, (uint64_t) fd, (uint64_t) flags);
+
+        if (res < 0) {
+            return -res;
+        }
+
+        *newfd = res;
+
+        return 0;
+    }
+
+    int sys_dup2(int fd, int flags, int newfd) {
+        ssize_t res = syscalln3(SYS_DUP2, (uint64_t) fd, (uint64_t) newfd, (uint64_t) flags);
+
+        if (res < 0) {
+            return -res;
+        }
+
+        return 0;
+    }
+
+    int sys_fcntl(int fd, int request, va_list args, int *result) {
+        ssize_t res = syscall(SYS_FCNTL, fd, request, va_arg(args, uint64_t));
+
+        if (res < 0) {
+            return -res;
+        }
+
+        *result = res;
+        return 0;
+    }
+
+    int sys_pselect(int nfds, fd_set *readfds, fd_set *writefds,
+                    fd_set *exceptfds, const struct timespec *timeout, const sigset_t *sigmask, int *num_events) {
+        ssize_t res = syscalln6(SYS_SELECT, (uint64_t) nfds, (uint64_t) readfds, (uint64_t) writefds,
+                                (uint64_t) exceptfds, (uint64_t) timeout, 0);
+
+        if (res < 0) {
+            return -res;
+        }
+
+        *num_events = res;
+
+        return 0;
+    }
+
+    int sys_chmod(const char *pathname, mode_t mode) {
+        return -1;
+    }
+
+    int sys_madvise(void *addr, size_t length, int advice) {
+        mlibc::infoLogger() << "mlibc: sys_madvise is a stub!" << frg::endlog;
+        return 0;
+    }
+
+    int sys_pipe(int *fds, int flags) {
+        ssize_t res = syscalln2(SYS_PIPE, (uint64_t) fds, (uint64_t) flags);
+
+        if (res < 0) {
+            return -res;
+        }
+
+        return 0;
+    }
+    //#endif
+} 
---
+++ sysdeps/cykusz/include/cykusz/syscall.h
@@ -0,0 +1,168 @@
+#ifndef SYSCALL_H
+#define SYSCALL_H
+
+#include <stdint.h>
+
+#define SYS_READ 0
+#define SYS_WRITE 1
+#define SYS_OPEN 2
+#define SYS_CLOSE 3
+#define SYS_CHDIR 4
+#define SYS_GETCWD 5
+#define SYS_MKDIR 6
+#define SYS_READDIR 7
+#define SYS_EXIT 8
+#define SYS_SLEEP 9
+#define SYS_BIND 13
+#define SYS_CONNECT 14
+#define SYS_SELECT 15
+#define SYS_MOUNT 16
+#define SYS_TIME 18
+#define SYS_SYMLINK 19
+#define SYS_RMDIR 20
+#define SYS_UNLINK 21
+#define SYS_LINK 22
+#define SYS_RENAME 23
+#define SYS_FORK 24
+#define SYS_EXEC 25
+#define SYS_FCNTL 26
+#define SYS_MMAP 27
+#define SYS_MUNMAP 28
+
+#define SYS_MAPS 29
+#define SYS_SEEK 30
+#define SYS_PREAD 31
+#define SYS_PWRITE 32
+
+#define SYS_WAITPID 33
+#define SYS_IOCTL 34
+#define SYS_SIGACTION 35
+#define SYS_SIGRETURN 36
+#define SYS_SIGPROCMASK 37
+
+#define SYS_FUTEX_WAIT 38
+#define SYS_FUTEX_WAKE 39
+#define SYS_ARCH_PRCTL 40
+#define SYS_SPAWN_THREAD 41
+#define SYS_EXIT_THREAD 42
+#define SYS_GETPID 43
+#define SYS_GETTID 44
+#define SYS_SETSID 45
+#define SYS_SETPGID 46
+#define SYS_PIPE 47
+#define SYS_DUP 48
+#define SYS_DUP2 49
+#define SYS_STAT 50
+#define SYS_READLINK 51
+#define SYS_GETRLIMIT 52
+#define SYS_DEBUG 53
+#define SYS_ACCESS 54
+#define SYS_KILL 55
+#define SYS_SYNC 56
+#define SYS_FSYNC 57
+#define SYS_TICKSNS 58
+#define SYS_GETPPID 59
+#define SYS_GETPGID 60
+#define SYS_TRUNCATE 61
+#define SYS_POLL 62
+#define SYS_SOCKET 63
+#define SYS_ACCEPT 64
+#define SYS_LISTEN 65
+#define SYS_MSGRECV 66
+#define SYS_MSGSEND 67
+#define SYS_SETSOCKOPT 68
+#define SYS_GETSOCKOPT 69
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+__attribute__((always_inline))
+static inline long syscalln0(uint64_t call) {
+    volatile long ret;
+    asm volatile ("syscall" : "=a"(ret)
+            : "a"(call)
+            : "rcx", "r11", "memory");
+    return ret;
+}
+
+__attribute__((always_inline))
+static long syscalln1(uint64_t call, uint64_t arg0) {
+    volatile long ret;
+    asm volatile ("syscall" : "=a"(ret)
+	    : "a"(call), "D"(arg0)
+	    : "rcx", "r11", "memory");
+    return ret;
+}
+
+__attribute__((always_inline))
+static long syscalln2(uint64_t call, uint64_t arg0, uint64_t arg1) {
+    volatile long ret;
+    asm volatile ("syscall" : "=a"(ret)
+	    : "a"(call), "D"(arg0), "S"(arg1)
+	    : "rcx", "r11", "memory");
+    return ret;
+}
+
+__attribute__((always_inline))
+static long syscalln3(uint64_t call, uint64_t arg0, uint64_t arg1, uint64_t arg2) {
+    volatile long ret;
+    asm volatile ("syscall" : "=a"(ret)
+            : "a"(call), "D"(arg0), "S"(arg1), "d"(arg2)
+            : "rcx", "r11", "memory");
+    return ret;
+}
+
+__attribute__((always_inline))
+static long syscalln4(uint64_t call, uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3) {
+    volatile long ret;
+    register uint64_t arg3r asm("r10") = arg3; // put arg3 in r10
+    asm volatile ("syscall" : "=a"(ret)
+	    : "a"(call), "D"(arg0), "S"(arg1), "d"(arg2), "r"(arg3r)
+	    : "rcx", "r11", "memory");
+    return ret;
+}
+
+__attribute__((always_inline))
+static long syscalln5(uint64_t call, uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4) {
+    volatile long ret;
+    register uint64_t arg3r asm("r10") = arg3; // put arg3 in r10
+    register uint64_t arg4r asm("r8") = arg4; // put arg4 in r9
+    asm volatile ("syscall" : "=a"(ret)
+	    : "a"(call), "D"(arg0), "S"(arg1), "d"(arg2), "r"(arg3r), "r"(arg4r)
+	    : "rcx", "r11", "memory");
+    return ret;
+}
+
+__attribute__((always_inline))
+static long syscalln6(uint64_t call, uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5) {
+    volatile long ret;
+    register uint64_t arg3r asm("r10") = arg3; // put arg3 in r10
+    register uint64_t arg4r asm("r8") = arg4; // put arg4 in r9
+    register uint64_t arg5r asm("r9") = arg5; // put arg5 in r8
+    asm volatile ("syscall" : "=a"(ret)
+	    : "a"(call), "D"(arg0), "S"(arg1), "d"(arg2), "r"(arg3r), "r"(arg4r), "r"(arg5r)
+	    : "rcx", "r11", "memory");
+    return ret;
+}
+
+#ifdef __cplusplus
+}
+    __attribute__((always_inline)) static inline long _syscall(uint64_t call) { return syscalln0(call); }
+    __attribute__((always_inline)) static inline long _syscall(uint64_t call, uint64_t arg0) { return syscalln1(call, arg0); }
+    __attribute__((always_inline)) static inline long _syscall(uint64_t call, uint64_t arg0, uint64_t arg1) { return syscalln2(call, arg0, arg1); }
+    __attribute__((always_inline)) static inline long _syscall(uint64_t call, uint64_t arg0, uint64_t arg1, uint64_t arg2) { return syscalln3(call, arg0, arg1, arg2); }
+    __attribute__((always_inline)) static inline long _syscall(uint64_t call, uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3) { return syscalln4(call, arg0, arg1, arg2, arg3); }
+    __attribute__((always_inline)) static inline long _syscall(uint64_t call, uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4)  { return syscalln5(call, arg0, arg1, arg2, arg3, arg4); }
+    __attribute__((always_inline)) static inline long _syscall(uint64_t call, uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, uint64_t arg5)  { return syscalln6(call, arg0, arg1, arg2, arg3, arg4, arg5); }
+
+    template<typename... T>
+    __attribute__((always_inline)) static inline long syscall(uint64_t call, T... args){
+        return _syscall(call, (uint64_t)(args)...);
+    }
+#else
+    #define GET_SYSCALL(a0, a1, a2, a3, a4, a5, a6, name, ...) name
+    #define syscall(...) GET_SYSCALL(__VA_ARGS__, syscalln6, syscalln5, syscalln4, syscalln3, syscalln2, syscalln1, syscalln0)(__VA_ARGS__) 
+#endif
+
+#endif
